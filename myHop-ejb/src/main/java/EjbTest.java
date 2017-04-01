0637fd4b67bcc04fb513d37ec46dce818eea4d87
Ningayo <Ningayo@192.168.1.97>
2017-04-02 00:56:20 +0200

    initial ejb

---
+++ myHop-ejb/src/main/java/EjbTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ningayo
+ *
+ */
+public class EjbTest {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("ejb module ....");
+	}
+
+}
---
+++ myHop-shared/src/main/java/SharedTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ningayo
+ *
+ */
+public class SharedTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Shared projet");
+
+	}
+
+}
